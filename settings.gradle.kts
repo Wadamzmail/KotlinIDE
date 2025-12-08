ff7d34db66dd919b1963027520f1179def236e58
Wadamzmail <alksndrmetwakil@gmail.com>
2025-12-08 09:23:18 +0200

    t

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
         maven("https://central.sonatype.com/repository/maven-snapshots")
         maven(url = "https://repo.gradle.org/gradle/libs-releases")
         maven(url = "https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://cache-redirector.jetbrains.com/kotlin.bintray.com/kotlin-plugin")
     }
 }
 
@@ -19,6 +20,7 @@ dependencyResolutionManagement {
         maven("https://central.sonatype.com/repository/maven-snapshots")
         maven(url = "https://repo.gradle.org/gradle/libs-releases")
         maven(url = "https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://cache-redirector.jetbrains.com/kotlin.bintray.com/kotlin-plugin")
     }
 }
 
